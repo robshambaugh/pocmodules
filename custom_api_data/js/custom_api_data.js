55021175a1c67572163ff22d34533d48c644959e
Rob Shambaugh <rob.shambaugh@acquia.com>
2024-08-04 14:05:24 -0400

    trying to fix cookies

--- custom_api_data/js/custom_api_data.js
+++ custom_api_data/js/custom_api_data.js
@@ -1,23 +1,23 @@
 (function() {
-    function setCookie(name, value, days) {
-      var expires = "";
-      if (days) {
-        var date = new Date();
-        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-        expires = "; expires=" + date.toUTCString();
-      }
-      document.cookie = name + "=" + (value || "") + expires + "; path=/";
+  function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+      var date = new Date();
+      date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+      expires = "; expires=" + date.toUTCString();
     }
-  
-    // Fetch customer data from a global variable set by PHP
-    var customerData = window.customerData;
-  
-    if (customerData) {
-      setCookie('customerFirstName', customerData.customerFirstName, 7);
-      setCookie('numberOfTrips', customerData.numberOfTrips, 7);
-      setCookie('tripName', customerData.tripName, 7);
-      setCookie('tripStartDate', customerData.tripStartDate, 7);
-      setCookie('tripEndDate', customerData.tripEndDate, 7);
-    }
-  })();
-  
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+  }
+
+  // Fetch customer data from a global variable set by PHP
+  var customerData = drupalSettings.customerData;
+  console.log('Customer Data:', customerData); // Debugging log
+
+  if (customerData) {
+    setCookie('customerFirstName', customerData.customerFirstName, 7);
+    setCookie('numberOfTrips', customerData.numberOfTrips, 7);
+    setCookie('tripName', customerData.tripName, 7);
+    setCookie('tripStartDate', customerData.tripStartDate, 7);
+    setCookie('tripEndDate', customerData.tripEndDate, 7);
+  }
+})();
