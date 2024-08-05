418a3fca419733c72972ba7130ce377cf735fc7d
Rob Shambaugh <rob.shambaugh@acquia.com>
2024-08-05 01:52:03 -0400

    Adding MinimalBlock to test .js

--- custom_api_data/js/custom_api_data.js
+++ custom_api_data/js/custom_api_data.js
@@ -1,14 +1,7 @@
 (function ($, Drupal, drupalSettings) {
   Drupal.behaviors.customApiData = {
     attach: function (context, settings) {
-      console.log("Drupal settings:", drupalSettings); // Log all drupalSettings
-      var customerData = drupalSettings.customApiData;
-      if (customerData) {
-        document.cookie = "customerData=" + JSON.stringify(customerData) + ";path=/;max-age=" + (86400 * 30); // 86400 = 1 day, cookie expires in 30 days
-        console.log("Customer Data cookie set:", customerData);
-      } else {
-        console.log("Customer Data not found in drupalSettings.");
-      }
+      console.log("Custom API Data JavaScript loaded");
     }
   };
 })(jQuery, Drupal, drupalSettings);
