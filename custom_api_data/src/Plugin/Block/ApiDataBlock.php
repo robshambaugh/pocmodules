76cdce28b256d814d93b849327eb880e5056f921
Rob Shambaugh <rob.shambaugh@acquia.com>
2024-08-04 23:18:42 -0400

    Update ApiDataBlock.php

--- custom_api_data/src/Plugin/Block/ApiDataBlock.php
+++ custom_api_data/src/Plugin/Block/ApiDataBlock.php
@@ -6,6 +6,7 @@ use Drupal\Core\Block\BlockBase;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use GuzzleHttp\ClientInterface;
+use GuzzleHttp\Exception\RequestException;
 
 /**
  * Provides a block to display API data.
@@ -34,7 +35,7 @@ class ApiDataBlock extends BlockBase implements ContainerFactoryPluginInterface 
   }
 
   public function build() {
-    // Replace with the correct URL for your API source site
+    $customer_id = 99999;
     $url = "https://robsapisource.demo.acsitefactory.com/jsonapi/node/customers";
     try {
       $response = $this->httpClient->request('GET', $url);
@@ -45,11 +46,15 @@ class ApiDataBlock extends BlockBase implements ContainerFactoryPluginInterface 
 
       if (isset($data['data']) && is_array($data['data'])) {
         foreach ($data['data'] as $customer) {
-          \Drupal::logger('custom_api_data')->info('Checking Customer ID: @id', ['@id' => $customer['attributes']['field_customer_id']]);
-          if (isset($customer['attributes']['field_customer_id']) && $customer['attributes']['field_customer_id'] == 99999) { // Replace 99999 with the actual ID
+          if (isset($customer['attributes']['field_customer_id']) && $customer['attributes']['field_customer_id'] == $customer_id) {
+            $customer_first_name = $customer['attributes']['field_customer_first_name'];
+            $customer_last_name = $customer['attributes']['field_customer_last_name'];
+            $trips_booked = $customer['attributes']['field_trips_booked'];
             return [
-              '#markup' => $this->t('Customer Data: @customer_data', [
-                '@customer_data' => json_encode($customer),
+              '#markup' => $this->t('Customer: @first_name @last_name<br>Trips Booked: @trips_booked', [
+                '@first_name' => $customer_first_name,
+                '@last_name' => $customer_last_name,
+                '@trips_booked' => $trips_booked,
               ]),
             ];
           }
