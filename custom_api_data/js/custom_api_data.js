b4d8b8d5430918e2e665dfbab7589f5e00742d8d
Rob Shambaugh <rob.shambaugh@acquia.com>
2024-08-05 00:09:03 -0400

    Update custom_api_data.js

--- custom_api_data/js/custom_api_data.js
+++ custom_api_data/js/custom_api_data.js
@@ -1,10 +1,13 @@
 (function ($, Drupal, drupalSettings) {
   Drupal.behaviors.customApiData = {
     attach: function (context, settings) {
+      console.log("Drupal settings:", drupalSettings); // Log all drupalSettings
       var customerData = drupalSettings.customApiData;
       if (customerData) {
         document.cookie = "customerData=" + JSON.stringify(customerData) + ";path=/;max-age=" + (86400 * 30); // 86400 = 1 day, cookie expires in 30 days
         console.log("Customer Data cookie set:", customerData);
+      } else {
+        console.log("Customer Data not found in drupalSettings.");
       }
     }
   };
